b721234dc055f9a72c4267b23d06e0b9a96b21fb
zheen <suseyizheen@gmail.com>
2025-05-24 17:05:44 -0700

    flash cards app WIP

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, Text, View, Image } from "react-native";
 export default function Index() {
   const num = 1
+  /*to do
+    have the flashcard in the middle with all of the functionality, 
+    when user touches card, it flips over 
+    arrows on the sides to change questions
+
+  */
   return (
-    <View className="flex-1 bg-blue-500">
-      <SafeAreaView className="flex-1 justify-start">
-        <View className="items-center">
-          <Text className="font-bold">Flash Cards</Text>
-        </View>
-      </SafeAreaView>
-    </View>
+    <SafeAreaView className="flex-1 justify-start bg-blue-500">
+      <Text className="text-2xl font-bold text-center">Easy Ace FlashCards</Text>
+      <View className="flex-row justify-end">
+        <Text>Randomize</Text>
+        <Image
+          source={require('../../assets/images/favicon.png')}
+        />
+      </View>
+      <View className="flex-1 justify-end p-12">
+        <Text className="text-center text-2xl font-bold">Card Number 8/14</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
